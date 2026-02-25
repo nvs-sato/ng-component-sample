fd50070ddcba5e071497648477fb24f5e6e1305b
a.sato <a.sato@nvs-net.jp>
2026-02-25 13:31:44 +0900

    新機能：受注一覧コンポーネントを追加し、AG Gridの設定を実装

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JyucyuListComponent } from './jyucyu/jyucyu-list/jyucyu-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'jyucyu/list', pathMatch: 'full' },
+  { path: 'jyucyu/list', component: JyucyuListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AgGridModule } from 'ag-grid-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JyucyuListComponent } from './jyucyu/jyucyu-list/jyucyu-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, JyucyuListComponent],
   imports: [BrowserModule, AppRoutingModule, AgGridModule],
   providers: [],
   bootstrap: [AppComponent]
